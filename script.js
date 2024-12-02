998fb427f34ada79123375667d46756b987d5e0c
bhawana-web <bhawnav161102@gmail.com>
2024-12-02 21:53:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Remove Color from Dropdown</title>
-</head>
-<body>
-    <!-- Dropdown list with id "colorSelect" -->
-    <select id="colorSelect">
-        <option value="Red">Red</option>
-        <option value="Green">Green</option>
-        <option value="White">White</option>
-        <option value="Black">Black</option>
-    </select>
-
-    <!-- Button that triggers removal of the selected color -->
-    <input type="button" id="removeButton" value="Select and Remove" onclick="removeColor()">
+// Function to remove the selected color from the dropdown
+function removeColor() {
+    // Get the select element by its ID
+    const colorSelect = document.getElementById('colorSelect');
     
-    <script src="main.js"></script>
-</body>
-</html>
+    // Get the selected option (the option that the user selected)
+    const selectedOption = colorSelect.options[colorSelect.selectedIndex];
+    
+    // If an option is selected, remove it
+    if (selectedOption) {
+        colorSelect.removeChild(selectedOption);
+    }
+}
