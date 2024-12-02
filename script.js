b3588a04299a10a8eac975350325e90851361f11
bhawana-web <bhawnav161102@gmail.com>
2024-12-02 21:44:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+// Function to remove the selected color from the dropdown
+function removeColor() {
+    // Get the select element by its ID
+    const colorSelect = document.getElementById('colorSelect');
+    
+    // Get the selected option (the option that the user selected)
+    const selectedOption = colorSelect.options[colorSelect.selectedIndex];
+    
+    // If an option is selected, remove it
+    if (selectedOption) {
+        colorSelect.removeChild(selectedOption);
+    }
+}
